0669c8f772da828b727bef801a5b26c4105d968c
SolaKabuta <solakabuta@gmail.com>
2025-02-18 15:40:37 +0100

    fix: title variable syntax fixed

--- templates/inc/navIncView.php
+++ templates/inc/navIncView.php
@@ -4,7 +4,7 @@
 
 <ul>
     <li><a href="./">Accueil</a></li>
-    <li><a href="./?p=apropos">Apropos</a></li>
-    <li><a href="./?p=histoire">Histoire</a></li>
+    <li><a href="./?p=aboutme">Apropos</a></li>
+    <li><a href="./?p=history">Histoire</a></li>
     <li><a href="./?p=contact">Contact</a></li>
 </ul>
